d2b87b194aa09df37a403089b4edb5b171946ea0
insightmediavn <hoaluholdings@gmail.com>
2025-07-08 16:46:49 +0700

    Update process-script.js

--- api/process-script.js
+++ api/process-script.js
@@ -1,3 +1,12 @@
+export const config = {
+  runtime: 'nodejs'
+};
+
+export default async function handler(req, res) {
+  ...
+}
+
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
